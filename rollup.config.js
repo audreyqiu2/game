6fc30399056c181fd5e19fd331366e2c69504328
audreyqiu2 <audreyqiu2@gmail.com>
2023-02-21 00:09:13 -0800

    finished game

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import copy from "rollup-plugin-copy";
 
 module.exports = {
-  input: "index.js",
+  input: "sketch.js",
   output: {
     dir: "dist",
   },
   plugins: [
     copy({
-      targets: [{ src: "index.html", dest: "dist" }],
+      targets: [{ src: ["index.html", "sketch_style.css", "sketch.js"], dest: "dist" }],
     }),
     nodeResolve(),
   ],
